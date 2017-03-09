7a71d336caaa08f7a38f1869436ae6a897329937
shya1386 <shyami.r86@gmail.com>
2017-03-09 17:07:28 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ import java.lang.*;
 import java.io.*;
  public class Test 
  {	
- static double W_Flatbase,W_numperson,W_value,O_price;
+  static double W_Flatbase,W_numperson,W_value,O_price;
   static double flat_rate=0.05;
   static double person_rate=0.012;
   static double food_rate=0.13;
@@ -20,25 +20,23 @@ import java.io.*;
   static String I_category2="drugs";
   static String I_category3="books";
  
-	 public static void main(String []args)
+  public static void main(String []args)
 	 {
-		Test ob=new Test();
+	Test ob=new Test();
         System.out.println("Input1:"+I_base1);
         System.out.println("Output1:"+ob.calculateprice(I_base1,I_numperson1,I_category1));
         System.out.println();
         System.out.println("Input2:"+I_base2);
-		System.out.println("Output2:"+ob.calculateprice(I_base2,I_numperson2,I_category2));
+        System.out.println("Output2:"+ob.calculateprice(I_base2,I_numperson2,I_category2));
         System.out.println();
         System.out.println("Input3:"+I_base3);
-		System.out.println("Output3:"+ob.calculateprice(I_base3,I_numperson3,I_category3));
- 
-     }
- 
-  	 public static double calculateprice(double base,int numperson,String category)
+	System.out.println("Output3:"+ob.calculateprice(I_base3,I_numperson3,I_category3));
+         }
+   public static double calculateprice(double base,int numperson,String category)
 	  	 {
       		 	//W_value=0.00;
        			W_Flatbase=base +(flat_rate*base);
-		 		W_numperson=person_rate*numperson*W_Flatbase;
+		 	W_numperson=person_rate*numperson*W_Flatbase;
 		 		if(category==("food")){
 		 		W_value  = food_rate * W_Flatbase;
 		 		O_price=W_Flatbase+W_numperson+W_value;
